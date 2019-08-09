05bdd9d50f1c78c71b81c263a8a7b345ba756fab
sergiimatusEPAM <49905012+sergiimatusEPAM@users.noreply.github.com>
2019-08-09 18:21:58 +0300

    Update winrm_setup.ps1

--- winrm_setup.ps1
+++ winrm_setup.ps1
@@ -1,6 +1,18 @@
-$winrm_cmd = 'winrm quickconfig -q & winrm set winrm/config @{MaxTimeoutms="1800000"} & winrm set winrm/config/service @{AllowUnencrypted="true"} & winrm set winrm/config/service/auth @{Basic="true"}'
+$winrm_cmd = {
+	winrm quickconfig -q 
+	winrm set winrm/config '@{MaxTimeoutms="1800000"}'
+	winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+	winrm set winrm/config/service/auth '@{Basic="true"}'
+}
+
+# Configure WinRM on Azure Windows instance
 & $winrm_cmd
-New-SelfSignedCertificate -DnsName $(Invoke-RestMethod -uri http://169.254.169.254/latest/meta-data/local-hostname) -CertStoreLocation Cert:\LocalMachine\My
-New-Item WSMan:\localhost\Listener -Address * -Transport HTTPS -HostName $(Invoke-RestMethod -uri http://169.254.169.254/latest/meta-data/local-hostname) -CertificateThumbPrint $(ls Cert:\LocalMachine\My).Thumbprint -Force
+
+
+$hostname = $(Invoke-RestMethod -Headers @{"Metadata"="true"} -URI "http://169.254.169.254/metadata/instance/compute/name?api-version=2019-02-01&&format=text")
+
+# Generate self signed certificate for WinRM usage
+New-SelfSignedCertificate -DnsName $hostname -CertStoreLocation Cert:\LocalMachine\My
+New-Item WSMan:\localhost\Listener -Address * -Transport HTTPS -HostName $hostname -CertificateThumbPrint $(ls Cert:\LocalMachine\My).Thumbprint -Force
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 Set-MpPreference -DisableRealtimeMonitoring $true
